332f38f7112887b3c53c135c32d43ceafe5f5d5e
mattcbodily <mattcbodily@gmail.com>
2021-03-02 16:31:08 -0700

    crypto currency prices now display

--- src/Components/CryptoPrices/CryptoPrices.js
+++ src/Components/CryptoPrices/CryptoPrices.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './CryptoPrices.css'
 
 const CryptoPrices = () => {
+    let [prices, setPrices] = useState({})
+
+    useEffect(() => {
+        axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum,bitcoin,litecoin&vs_currencies=usd')
+        .then(res => setPrices(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    console.log(prices)
+
     return (
         <section id='crypto-flex'>
-            <div className='crypto-display'>Crypto 1</div>
-            <div className='crypto-display'>Crypto 2</div>
-            <div className='crypto-display'>Crypto 3</div>
+            <div className='crypto-display'>BTC ${prices.bitcoin?.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+            <div className='crypto-display'>ETH ${prices.ethereum?.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+            <div className='crypto-display'>LTC ${prices.litecoin?.usd.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
         </section>
     )
 }
