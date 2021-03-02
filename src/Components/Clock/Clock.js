41c1180e66b7601ac6b35b663a06a11195f6a90c
mattcbodily <mattcbodily@gmail.com>
2021-03-02 09:37:28 -0700

    added clock functionality

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Clock.css'
 
 const Clock = () => {
+    let [time, setTime] = useState('')
+
+    const updateTime = num => {
+        if(num < 10){
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    const currentTime = () => {
+        let date = new Date();
+        let hour = date.getHours();
+        let min = date.getMinutes();
+        let timeOfDay = 'AM';
+        timeOfDay = hour >= 12 ? 'PM' : 'AM'
+    
+        hour = (hour === 0) ? 12 : (hour > 12) ? hour - 12 : hour;
+        hour = updateTime(hour)
+        min = updateTime(min)
+
+        setTime(`${hour}:${min} ${timeOfDay}`)
+    }
+
+    useEffect(() => {
+        currentTime()
+        setTimeout(function(){ currentTime() }, 1000)
+    })
+
     return (
-        <section></section>
+        <section id='clock-display'>{time}</section>
     )
 }
 
