e04c55590369d5e9cb845678c8606c264019c5a4
mattcbodily <mattcbodily@gmail.com>
2021-03-02 09:30:03 -0700

    date functionality added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.css'
 
 const Header = () => {
+    let [date, setDate] = useState('')
+
+    useEffect(() => {
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth() + 1;
+        setDate(`${mm}/${dd}`);
+    }, [date])
+
     return (
         <header>
-            <section id='date-display' className='header-compartment'>Date Here</section>
+            <section id='date-display' className='header-compartment'>{date}</section>
             <section className='header-compartment'>Weather Here</section>
         </header>
     )
