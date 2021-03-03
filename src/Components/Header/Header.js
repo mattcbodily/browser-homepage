e07db7aa7697111f9b0fd84312b5bad42d299bfa
mattcbodily <mattcbodily@gmail.com>
2021-03-03 12:14:27 -0700

    adjusting comparisons in Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -18,11 +18,11 @@ const Header = () => {
 
         let j = day % 10
         let k = day % 100
-        if (j == 1 && k != 11) {
+        if (j === 1 && k !== 11) {
             day += "st";
-        } else if (j == 2 && k != 12) {
+        } else if (j === 2 && k !== 12) {
             day += "nd";
-        } else if (j == 3 && k != 13) {
+        } else if (j === 3 && k !== 13) {
             day += "rd";
         } else {
             day += "th"
