a30944ca3d571631b1773e6ed0d1021577b4f255
mattcbodily <mattcbodily@gmail.com>
2021-03-12 15:28:14 -0700

    adding jump logic

--- src/Components/Game/gameLogic.js
+++ src/Components/Game/gameLogic.js
@@ -1,8 +1,22 @@
-const player = {
-    x: 15,
-    y: 200,
-    height: 25,
-    width: 25
+const gravity = .5
+let jumpActive = false
+
+class Player {
+    constructor(){
+        this.x = 15;
+        this.y = 200;
+        this.height = 25;
+        this.width = 25;
+        this.jumpVelocity = 30;
+    }
+
+    jump(){
+        if(!jumpActive){
+            jumpActive = true;
+            this.y -= this.jumpVelocity
+        }
+        jumpActive = false
+    }
 }
 
 class Obstacle {
